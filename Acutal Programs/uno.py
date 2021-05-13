943e49aa8ccc6adf20b86ae0e0664a9f93ea9137
Christopher Shariff <CShariff@albany.edu>
2021-05-13 14:24:30 -0400

    Building deck

---
+++ Acutal Programs/uno.py
@@ -0,0 +1,15 @@
+def buildDeck():
+    deck =[]
+    colors = ["Blue", "Green", "Yellow", "Red"]
+    values = ["Draw Two", "Skip", "Reverse", 0,1,2,3,4,5,6,7,8,9]
+    wildcards = ["Wild", "Wild Draw Four"]
+    for color in colors:
+        for value in values:
+            cardvalue = "{} {}".format(color,value)
+            deck.append(cardvalue)
+            if value != 0:
+                deck.append(cardvalue)
+    for i in range(4):
+        deck.append(wildcards[0])
+        deck.append(wildcards[1])
+    return deck
