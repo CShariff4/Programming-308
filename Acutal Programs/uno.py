4eb703dccba8cf7b3797d2d1319617280151f77d
Christopher Shariff <CShariff@albany.edu>
2021-05-13 14:29:41 -0400

    defined shuffleDeck

--- Acutal Programs/uno.py
+++ Acutal Programs/uno.py
@@ -17,6 +17,13 @@ def buildDeck():
     return deck
 
 
-    def shuffleDeck(deck):
-        for cardPosition in range(len(deck)):
-    unoDeck = buildDeck()
+def shuffleDeck(deck):
+    for cardPosition in range(len(deck)):
+        randPos = random.randint(0,107)
+        deck[cardPos], deck[randPos] = deck[randPos], deck[cardPos]
+        return deck
+
+
+
+
+unoDeck = buildDeck()
